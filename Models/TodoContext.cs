545bb26da7004e9044913b9710c2e124a091c8c0
francis <francisalcantara001@gmail.com>
2021-12-21 15:55:15 -0500

    webapi with DTO

---
+++ Models/TodoContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
